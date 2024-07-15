3b2b6306a72af0e54bbf7ed333c36ce0bf9e07c2
DPE Anatolii Klots <anatolii.klots@dominos.nl>
2024-07-15 11:17:21 +0200

    Adding new board + SD card config

--- src/sd_files.cpp
+++ src/sd_files.cpp
@@ -14,15 +14,15 @@
 
 #include "files.h"
 
+#ifdef SD_CS_PIN
+#define SD SD_MMC
+#endif
+
 class ArduinoReadFileStream : public IReadStream {
     public:
         ArduinoReadFileStream(const char* fileName) 
         {
-            #ifdef SD_CS_PIN
             _file = SD.open(fileName);
-            #else
-            _file = SD_MMC.open(fileName);
-            #endif
            _stream  = new ReadBufferingStream {_file, 2000};
         }
 
@@ -54,64 +54,67 @@ class ArduinoFileStreamFactory : public IFileSystem {
         std::string _root;
 };
 
-#ifdef SD_CS_PIN
+// bool init_file_system() 
+// {
+//     if (!SD.begin(SD_CS_PIN)) {
+//         log_e("Card Mount Failed");
+//        //Serial.println();
+//         return false;
+//     }
+//     uint8_t cardType = SD.cardType();
+
+//     if (cardType == CARD_NONE) {
+//         log_e("No SD card attached");
+//        //Serial.println("No SD card attached");
+//         header_msg("No SD card attached");
+//         return false;
+//     }
+
+//     log_i("SD Card Type:");
+//     //Serial.print("SD Card Type: ");
+//     if (cardType == CARD_MMC) {
+//         //Serial.println("MMC");
+//         log_i("MMC");
+//     } else if (cardType == CARD_SD) {
+//         //Serial.println("SDSC");
+//         log_i("SDSC");
+//     } else if (cardType == CARD_SDHC) {
+//         //Serial.println("SDHC");
+//         log_i("SDHC");
+//     } else {
+//         //Serial.println("UNKNOWN");
+//         log_i("UNKNOWN");
+//     }
+
+//     uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+//    // Serial.printf("SD Card Size: %lluMB\n", cardSize);   
+//     //Serial.println("initialisation done.");
+//     log_i("SD Card Size: %lluMB\ninitialisation done.\n", cardSize);
+//     return true;
+// }
 
 bool init_file_system() 
 {
-    if (!SD.begin(SD_CS_PIN)) {
-        log_e("Card Mount Failed");
-       //Serial.println();
-        return false;
-    }
-    uint8_t cardType = SD.cardType();
-
-    if (cardType == CARD_NONE) {
-        log_e("No SD card attached");
-       //Serial.println("No SD card attached");
-        header_msg("No SD card attached");
-        return false;
-    }
-
-    log_i("SD Card Type:");
-    //Serial.print("SD Card Type: ");
-    if (cardType == CARD_MMC) {
-        //Serial.println("MMC");
-        log_i("MMC");
-    } else if (cardType == CARD_SD) {
-        //Serial.println("SDSC");
-        log_i("SDSC");
-    } else if (cardType == CARD_SDHC) {
-        //Serial.println("SDHC");
-        log_i("SDHC");
-    } else {
-        //Serial.println("UNKNOWN");
-        log_i("UNKNOWN");
-    }
-
-    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-   // Serial.printf("SD Card Size: %lluMB\n", cardSize);   
-    //Serial.println("initialisation done.");
-    log_i("SD Card Size: %lluMB\ninitialisation done.\n", cardSize);
-    return true;
-}
-
-#else
-
-bool init_file_system() 
-{
-    if (!SD_MMC.setPins(SD_SCLK, SD_CMND, SD_DAT0, SD_DAT1, SD_DAT2, SD_DAT3)) {
+    #ifdef SD_DAT1
+    if (!SD.setPins(SD_SCLK, SD_CMND, SD_DAT0, SD_DAT1, SD_DAT2, SD_DAT3)) {
         log_e("Unable to set pins");
         return false;
     }
+    #else
+    if (!SD.setPins(SD_SCLK_PIN, SD_MOSI_PIN, SD_MISO_PIN)) {
+        log_e("Unable to set pins");
+        return false;
+    }
+    #endif
 
-    if (!SD_MMC.begin("/")) {
+    if (!SD.begin("/", true)) {
         log_e("Card Mount Failed");
         return false;
     }
 
     log_i("Card Mounted");
 
-    uint8_t cardType = SD_MMC.cardType();
+    uint8_t cardType = SD.cardType();
 
     if (cardType == CARD_NONE) {
         log_e("No SD card attached");
@@ -130,13 +133,11 @@ bool init_file_system()
         log_i("UNKNOWN");
     }
     
-    uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
     log_i("SD Card Size: %lluMB\ninitialisation done.\n", cardSize);
 
     return true;
 }
-
-#endif
 
 IFileSystem* get_file_system(const char* root) {
     return new ArduinoFileStreamFactory(root);
